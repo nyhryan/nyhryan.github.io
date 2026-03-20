8d9faba98ae2c300f338c06e92c0b95f30b3d04e
nyhryan <nyhryan99102@gmail.com>
2026-03-20 16:33:31 +0900

    chore: 🧹 Add test cases for External URL checker helper function

--- src/libs/remarkObsidianLink.test.ts
+++ src/libs/remarkObsidianLink.test.ts
@@ -6,9 +6,11 @@ interface Case {
   expected: boolean;
 }
 const inputUrls: Array<Case> = [
-  { url: "https://google.com", expected: true },
   { url: "../../relative-article.md", expected: false },
   { url: "./inner/some-article.md#heading-three", expected: false },
+  { url: "current/folder/some-article.mdx", expected: false },
+  { url: "mailto:someone@example.com", expected: true },
+  { url: "https://google.com", expected: true },
 ];
 
 test.each(inputUrls)(
